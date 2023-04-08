0d1874c47cb5251ac0399e4bf0d772dd1bde2afe
DESKTOP-RONIHRQ\Legion 5 <dinhsyduyet@gmail.com>
2023-04-08 15:25:05 +0700

    add env

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
-import Routes from './components/Routes.jsx';
+import Routes from './src/components/Routes.jsx';
 
 const container = document.getElementById('app');
 hydrateRoot(container, <Routes/>);
